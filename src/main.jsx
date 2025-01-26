8bc78bf1eb79eb2686678fa3a0abb9013f980b99
Torandori <vaviktova@gmail.com>
2025-01-26 21:13:43 +0100

    Removed sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './assets/scss/style.scss'
+import './assets/css/global.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
