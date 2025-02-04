1c673860aa5c0805574e5fdc2229e166ab86a46e
Torandori <vaviktova@gmail.com>
2025-02-04 23:43:29 +0100

    Minify JS, JSX, HTML, and CSS with Terser and htmlMinifier

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import htmlMinifier from 'vite-plugin-html-minifier';
 
 export default defineConfig({
   base: '/music_project/',
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    htmlMinifier({
+      minify: true,
+      removeComments: true,
+      collapseWhitespace: true,
+    }),
+  ],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true, 
+      },
+    },
+  },
+});
